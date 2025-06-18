c53fb83098fb972d62efe2b0e85ddfae9275f4eb
Marlon Ortiz <marlon.ortiz@celec.gob.ec>
2025-06-17 22:52:16 -0500

    ingresado nuevo boton

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,7 @@ export default function Home() {
         <a href="https://reportesdiarios.vercel.app/" className="button" target="_blank" rel="noopener noreferrer">Reportes Diarios de Mantenimiento</a>
         <a href="https://horasextras.vercel.app/" className="button" target="_blank" rel="noopener noreferrer">Ingreso de Horas Extras</a>
         <a href="https://turnosfiltros.vercel.app/" className="button" target="_blank" rel="noopener noreferrer">Turnos Limpieza de Filtros</a>
-        <a href="https://enviareportes.vercel.app/" className="button" target="_blank" rel="noopener noreferrer">Gestión de Reportes por Supervisor</a> */}
-
-     
+        <a href="https://enviareportes.vercel.app/" className="button" target="_blank" rel="noopener noreferrer">Gestión de Reportes por Supervisor</a>
 
       </main>
 
